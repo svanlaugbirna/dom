645f11d958933fc656191c23affeb0a77cd383e9
svanlaugbirna <svanlaugbirna@gmail.com>
2023-12-04 09:57:19 +0000

    testing the dom

---
+++ src/script.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const shoppingList = document.getElementById('shoppingList');
+  
+    function createListItem(itemName) {
+      const li = document.createElement('li');
+      li.innerHTML = `
+        <span>${itemName}</span>
+        <input type="number" value="1" min="1">
+        <button class="removeBtn">Remove</button>
+      `;
+  
+      const removeBtn = li.querySelector('.removeBtn');
+      removeBtn.addEventListener('click', function () {
+        li.remove();
+      });
+  
+      shoppingList.appendChild(li);
+    }
+  
+    const addItemForm = document.createElement('form');
+    addItemForm.innerHTML = `
+      <input type="text" id="itemName" placeholder="Enter item name" required>
+      <button type="submit">Add Item</button>
+    `;
+  
+    addItemForm.addEventListener('submit', function (e) {
+      e.preventDefault();
+      const itemNameInput = document.getElementById('itemName');
+      const itemName = itemNameInput.value.trim();
+      if (itemName !== '') {
+        createListItem(itemName);
+        itemNameInput.value = '';
+      }
+    });
+  
+    document.body.insertBefore(addItemForm, shoppingList);
+  });
+  
